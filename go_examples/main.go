62994c2d7bc36e37da8341b5f7ba6561f998e963
maulepilot117 <chris.white@rubrik.com>
2025-01-17 11:41:45 -0500

    Added string literal

--- go_examples/main.go
+++ go_examples/main.go
@@ -4,5 +4,9 @@ import "fmt"
 
 func main() {
 	const name, age = "Chris", 37
-	fmt.Printf("%s is %d years old.\n", name, age)
+	fmt.Printf("%s is %d years old. 😂\n", name, age)
+	
+	fmt.Printf(`	This is a raw string literal. 😜
+	We can put anything in here without newlines and escaping.
+	It's pretty legit.`)
 }
